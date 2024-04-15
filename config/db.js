3651451034ad2c6e6c1ced4e6ccefada1f0a068f
ALOK P <apurva2183@gmail.com>
2024-04-15 16:29:03 +0530

    deleted db

--- config/db.js
+++ config/db.js
@@ -5,13 +5,7 @@ import mysql from 'mysql2/promise';
  
   
 
-        const con = await mysql.createConnection({
-             host :'sql6.freesqldatabase.com',
-             user : 'sql6699140',
-             password: '1EuPq9pm25',
-             database: 'sql6699140'
-
-        });
+ 
         
     
 
